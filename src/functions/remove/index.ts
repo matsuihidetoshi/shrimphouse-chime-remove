ba8fc78046781a12a7dbb2870216b919657ce3a7
松井英俊 <dev.matsuihidetoshi@gmail.com>
2021-03-01 00:54:17 +0900

    remove api gateway

--- src/functions/remove/index.ts
+++ src/functions/remove/index.ts
@@ -1,18 +1,4 @@
-import schema from './schema';
-
 export default {
   handler: `${__dirname.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}/handler.main`,
-  events: [
-    {
-      http: {
-        method: 'post',
-        path: 'hello',
-        request: {
-          schema: {
-            'application/json': schema
-          }
-        }
-      }
-    }
-  ]
+  events: []
 }
